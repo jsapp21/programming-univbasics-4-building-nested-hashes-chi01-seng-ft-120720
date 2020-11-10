677607f436083ee47122da435ce3017f6c233662
James Sapp <jameseisapp@gmail.com>
2020-11-10 16:42:25 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -6,7 +6,7 @@ end
 
 def monopoly_with_second_tier
   return hash = { 
-    :railroads => { pieces: 4, rent_in_dollars: {}, names: {} },
+    :railroads => { pieces: 4, rent_in_dollars: {}, names: {} }
   } 
 end
 
@@ -18,7 +18,8 @@ def monopoly_with_third_tier
   
   return hash = { 
     :railroads => { pieces: 4, 
-    rent_in_dollars: { one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 100, four_pieces_owned: 200 }, names: {} },
+    rent_in_dollars: { one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 100, four_pieces_owned: 200 }, 
+    names: { reading_railroad: {}, pennsylvania_railroad: {}, b_and_o_railroad: {}, shortline_railroad: {} } }
   } 
 
 end
